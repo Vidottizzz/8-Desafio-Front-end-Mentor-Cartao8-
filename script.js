982d874464158d68b3df38a7622976a065ae88c7
Vidottizzz <vidottizzz@gmail.com>
2021-12-21 05:48:51 -0300

    mobile-first e desktop mode completo, projeto finalizado

--- script.js
+++ script.js
@@ -1,24 +1,42 @@
 const defaultDiv = document.querySelector('footer');
+const buttonImg = document.querySelector('.footer-social')
 const secondDiv = document.querySelector('.wrap-footer-active');
 const button = document.querySelector('.social-div');
 const button2 = document.querySelector('.social-div-active');
+const popover = document.querySelector('.popover-content');
 
 secondDiv.classList.add('hide');
 
-button.addEventListener('click', ()=> {
+button.addEventListener('click', () => {
     // !defaultDiv.classList.contains('.hide') ?  defaultDiv.classList.add('hide') : secondDiv.classList.remove('hide');
+    let largura = window.innerWidth;
+    if (largura < 760) {
+        secondDiv.classList.toggle('hide')
+        defaultDiv.classList.toggle('hide');
+    }
+    if (largura > 760) {
+        secondDiv.classList.add('hide');
+        defaultDiv.classList.remove('hide')
+    }
+    popover.classList.toggle('show-pop')
 
-   
-           secondDiv.classList.toggle('hide')
-           defaultDiv.classList.toggle('hide');
-          
 });
 
-button2.addEventListener('click', ()=> {
-
+button2.addEventListener('click', () => {
     secondDiv.classList.toggle('hide');
     defaultDiv.classList.toggle('hide');
-
 });
 
+window.addEventListener('resize', function () {
+    let largura = window.innerWidth;
 
+    if (largura > 760) {
+        secondDiv.classList.add('hide');
+        defaultDiv.classList.remove('hide')
+        popover.classList.remove('show-pop');
+        buttonImg.classList.add('filter-white');
+        buttonImg.parentElement.className.add('bg-blue-darker');
+    } else {
+        buttonImg.classList.remove('filter-white');
+    }
+});
